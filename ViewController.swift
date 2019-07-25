60d2e7199a43cdf217917c18b36aab0b1dc8e197
Girish k <girimarbh@gmail.com>
2019-07-25 17:19:47 +0530

    first commit

---
+++ ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Sampleios
+//
+//  Created by imgadmin on 25/07/19.
+//  Copyright © 2019 Wipro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hiiiiiiiiiii")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
